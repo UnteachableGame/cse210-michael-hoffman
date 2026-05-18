4e18cd076d5da37fde1f1981e84eaec53dd702b6
Michael Hoffman <82920939+UnteachableGame@users.noreply.github.com>
2026-05-18 13:04:43 -0600

    fractions updated

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,39 @@
+﻿namespace Learning03;
+
+public class Fraction {
+    private int top;
+    private int bottom;
+
+    public Fraction() {
+        top = 1;
+        bottom = 1;
+    }
+
+    public Fraction(int wholeNumber) {
+        top = wholeNumber;
+        bottom = 1;
+    }
+
+    public Fraction(int top, int bottom) {
+        this.top = top;
+        this.bottom = bottom;
+    }
+
+    public int Top {
+        get => top;
+        set => top = value;
+    }
+
+    public int Bottom {
+        get => bottom;
+        set => bottom = value;
+    }
+
+    public String getFractionString() {
+        return top + "/" + bottom;
+    }
+
+    public double getDoubleString() {
+        return top / bottom;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,5 +1,19 @@
+using Learning03;
+
 internal class Program {
     private static void Main(string[] args) {
         Console.WriteLine("Hello Learning03 World!");
+        
+        // Encapsulation
+
+        Fraction f = new Fraction();
+        Random ran = new Random();
+
+        for (int i = 0; i < 20; i++) {
+            f.Top = ran.Next();
+            f.Bottom = ran.Next();
+            
+            Console.WriteLine($"Fraction 1: string: {f.Top} / {f.Bottom} Number: {f.Top / f.Bottom}");
+        }
     }
 }
